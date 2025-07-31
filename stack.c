d035b7c6f5ec1813a7b57a50df7885408a5d2337
cuban-goats <erikkoston@mailfence.com>
2025-08-01 00:19:20 +0200

    added a stack with a push and pop function

---
+++ stack.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define STACK_LENGTH 20
+#define EMPTY (-1)
+
+typedef struct {
+  int stack_values[STACK_LENGTH];
+  int top;
+} stack;
+
+bool push(stack *mystack, int value) {
+  if (mystack->top >= STACK_LENGTH - 1)
+    return false;
+
+  mystack->top++;
+  mystack->stack_values[mystack->top] = value;
+  return true;
+}
+
+bool pop(stack *mystack) {
+  if (mystack->top == EMPTY)
+    return false;
+  
+  int result = mystack->stack_values[mystack->top];
+  mystack->top--;
+  return true;
+}
+
+int main() {
+  stack stack_one;
+  stack_one.top = -1;
+  push(&stack_one, 20);
+  return 0;
+}
